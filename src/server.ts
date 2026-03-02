1017326a263fca2b6b97d872cdab43f41772766b
Sergey Kornienko <kornienko955@gmail.com>
2026-03-02 15:41:37 +0300

    tgbot

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,14 @@ import app from './app';
 import config from './config/config';
 
 import { initDb } from './db/models';
+import { launchBot } from './tgBot';
 
 (async () => {
   await initDb();
+})();
+
+(async () => {
+  await launchBot();
 })();
 
 app.listen(config.port, () => {
