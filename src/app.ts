de6d2f4e43b82cbb95b7a90477c30bc3a04c1808
Sergey Kornienko <kornienko955@gmail.com>
2026-05-14 22:51:24 +0300

    Revert "rate limite"
    This reverts commit 62842127e681b02703f3199425a70153a7359716.

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,6 @@ import paymentUnauthorizedRoute from './routes/payment-unauthorized.routes';
 import { NotFoundError } from './utils/error.utils';
 import { errorHandler } from './middlewares/error.middleware';
 import { jwtAuthMiddleware } from './middlewares/jwt.middleware';
-import rateLimit from 'express-rate-limit';
 
 const corsOptions = {
   origin: true,
@@ -36,13 +35,6 @@ app.use(express.urlencoded({
 }));
 
 app.use(cors(corsOptions));
-
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 минут
-  max: 100, // максимум 100 запросов
-  message: 'Too many requests',
-  skipSuccessfulRequests: true, // не считать успешные
-});
 
 const protectedRoutes = [
   gratitudeRoute,
@@ -69,8 +61,6 @@ protectedRoutes.forEach((router) => {
 app.use('*', (_req, _res, next) => {
   next(new NotFoundError('Маршрут'));
 });
-
-app.use('/api', limiter);
 
 app.use(errorHandler);
 
